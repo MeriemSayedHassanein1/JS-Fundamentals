a918218c6a0c1599cf18ef5ea58a1ceb25c7aa71
MeriemSayedHassanein1 <elayki.bijouterie.manager@gmail.com>
2025-09-15 14:01:00 +0100

    update in exercice 6

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 let arraylist=["C is fun","Python is cool","JavaScript is amazing"];
 let output="";
-for(let i=0;i<arraylist.length;i++){
-    console.log(arraylist[i]);
+for (let i = 0; i < arraylist.length; i++) {
+  console.log(arraylist[i]);
 }
